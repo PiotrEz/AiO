00db48cd806ce23794989bc4b46c51099fd7c147
PiotrEz <panpiotruzieblo@gmail.com>
2022-08-03 17:24:46 +0200

    Yaml

---
+++ App3/BS-App3/main.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import jsonify
+import redis
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--rabbit_ip", help="Database IP address")
+parser.add_argument("-pr", "--port_redis", default="6379", help="Redis Database Port")
+parser.add_argument("-p", "--port", default="2500", help="Set application port")
+args = vars(parser.parse_args())
+
+
+REDIS_IP = args["rabbit_ip"]
+PORT_REDIS = args["port_redis"]
+APP_PORT = args["port"]
+
+
+app = Flask(__name__)
+r = redis.Redis(host=REDIS_IP, port=PORT_REDIS, db=0)
+
+
+@app.route('/size', methods=['GET'])
+def test_GET():
+    return jsonify(result=connection(REDIS_IP,PORT_REDIS))
+
+
+def connection(IP_ADDRES,PORT_REDIS):
+    return r.dbsize()
+
+app.run(host='0.0.0.0',port=APP_PORT)
